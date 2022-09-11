082cff0b33f1d37d9382fb6ba1d3891ce3f4659f
Benjamin Nguyen <ikarostdx@gmail.com>
2022-09-11 12:08:34 -0700

    added more test when the user create the profile

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -1,6 +1,9 @@
 """
 Test for the user API
 """
+import email
+from unicodedata import name
+from venv import create
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
@@ -12,16 +15,16 @@ from rest_framework import status
 CREATE_USER_URL = reverse('user:create')
 
 TOKEN_URL = reverse('user:token')
+ME_URL = reverse('user:me')
 
 
 def create_user(**params):
     """Create and return a new user"""
     return get_user_model().objects.create_user(**params)
 
-# Public Test - Unauthenticated request
-
 
 class PublicUserApiTests(TestCase):
+    # Public Test - Unauthenticated request
     """Test the public feature of the user API"""
 
     def setUp(self):
@@ -114,3 +117,52 @@ class PublicUserApiTests(TestCase):
 
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_user_unauthorized(self):
+        """Test authentication is required for users"""
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateUserApiTests(TestCase):
+    """Test API requests that require authentication"""
+
+    def setUp(self):
+        self.user = create_user(
+            email="test@example.com",
+            password="testpassword",
+            name="test user"
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_retrieve_profile_success(self):
+        """Test retrieving profile for logged in user"""
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, {
+            'name': self.user.name,
+            'email': self.user.email,
+        })
+
+    def test_post_me_not_allowed(self):
+        """Test POST ist not allowed for the me URL endpoint"""
+        # Me URL should only be used when we create object in the system
+        res = self.client.post(ME_URL, {})
+
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_update_user_profile(self):
+        """Test updating user profile for the authenticated user"""
+        pay_load = {
+            'name': "update name",
+            "password": "update-password",
+        }
+        res = self.client.patch(ME_URL, pay_load)
+
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.name, pay_load['name'])
+        self.assertEqual(self.user.check_password, pay_load['password'])
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
--- app/user/views.py
+++ app/user/views.py
@@ -1,7 +1,7 @@
 """
 Views for the user API
 """
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
@@ -21,3 +21,14 @@ class CreateTokenView(ObtainAuthToken):
     """Create a new authorization token in the system for the user"""
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user in the system"""
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """Return the authenticated user"""
+        return self.request.user
