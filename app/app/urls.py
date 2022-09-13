8b898c0e56f0080fc36d95f5f9123d863cc06255
benjix06 <ikarostdx@gmail.com>
2022-09-13 15:36:16 -0700

    Update code

--- app/app/urls.py
+++ app/app/urls.py
@@ -22,8 +22,11 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from core import views as core_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/health-check/', core_view.health_check, name='health-check'),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
 
     # User the schema to generate a graphical user interface
